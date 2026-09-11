d458eb7c7196aca3c0116efa70d149c1c436adec
Sabbir Hossain <sabbirhossainnubian@gmail.com>
2026-09-12 00:39:55 +0600

    first step

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero/Hero";
-import Technologies from "./components/Technologies/Technologies";
-import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Hero />
-      <Technologies />
-      <Footer />
+
     </>
   );
 }
